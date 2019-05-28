9bfdc6e8d22bb698bfe1b5d7e9368ce32630c5ee
Mohsin Razzaq <mohsinrazzaq25@gmail.com>
2019-05-29 02:04:38 +0500

    successfully login admin

--- loginForm-Page.php
+++ loginForm-Page.php
@@ -3,45 +3,31 @@
 <head>
   <meta charset="UTF-8">
   <title>Admin Login Page</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
-    <link rel="stylesheet" href="css/loginForm-style.css">
 </head>
 <?php
-include("db.php");
-error_reporting(0);
 if(isset($_POST['submit'])){
 $UserName =  $_POST['username'];
 $Password =  $_POST['password'];
+include("db.php");
 $query1 = "SELECT * FROM admin WHERE admin_name ='$UserName' AND admin_password = '$Password'";
 $res1 = mysqli_query($con,$query1); 
 $row = mysqli_fetch_array($res1);
 if($row!= ""){
   $_SESSION['userId'] = $row['admin_id'];
   $_SESSION['username'] = $row['admin_name'];
-  header("location:admin_page.html");
+  header("location:admin_page.php");
 }
 else{
   echo"Invalid username or password.";
   }
 }
-}
 ?>
 <body>
-  <div class="form-wrapper">
     <form action="" method="POST">
-      <div class="form">
-        <div class="username">
-          <input type="text" name="username" placeholder="USERNAME"/>
-        </div>
-        <div class="password">
-          <input type="text" name="password" placeholder="PASSWORD"/>
-        </div>
-        <div class="login">
-          <input type="button" name="submit" class="login btn" value="LOGIN">
-        </div>
-      </div> 
+  <input type="text" name="username"><br>
+  <input type="text" name="password"> <br>
+  <input type="submit" name="submit" value="login">
     </form>
-  </div>
 
  
 </body>
