79900a8d3275912a97b62e519c08428ef66bc8dd
Mohsin Razzaq <mohsinrazzaq25@gmail.com>
2019-06-01 23:10:05 +0500

    linking admin view funds page in navbar

--- admin_view_funds.php
+++ admin_view_funds.php
@@ -11,9 +11,53 @@ if($_SESSION['userId'] == '' )
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>admin view funds </title>
+    <title>admin view funds</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+<div class="wrapper">
+    <!-- navbar started -->
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+          <a class="navbar-brand" href="admin_page.php">AL-Hammd Travles and Tours</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="admin_page.php">Home</a></li>
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Users <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="admin_view_users.php">View Users</a></li>
+                <li><a href="admin_add_users.php">Add New Users</a></li>
+                <li><a href="#"></a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Funds <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="admin_add_funds.php">Add Funds</a></li>
+                <li><a href="admin_view_funds.php">View Added Funds</a></li>
+                <li><a href="#"></a></li>
+              </ul>
+            </li>
+            <li><a href="admin_update_account.php">Update Account</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- navbar ended  -->
+  </div>
 </body>
 </html>
