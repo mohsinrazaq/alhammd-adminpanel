51f189c708aef11472c0215bf7fa39f260ed106f
Mohsin Razzaq <mohsinrazzaq25@gmail.com>
2019-06-01 23:08:49 +0500

    working on admin view funds page

--- admin_view_funds.php
+++ admin_view_funds.php
@@ -1,10 +1,17 @@
+<?php
+include("db.php");
+if($_SESSION['userId'] == '' )
+{
+	header("location:loginForm-Page.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>admin view funds </title>
 </head>
 <body>
     
